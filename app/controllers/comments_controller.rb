d56808adcc5147e5a7bfb06aafd40fc811bd68f2
Takahiro Suzuki <takahirosuzuki.k010@icloud.com>
2017-04-19 10:30:16 +1000

    2nd commit as AddImageComment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-  
+
 
     respond_to do |format|
       if @comment.save
@@ -75,6 +75,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:user_id, :title, :content, :post_id)
+      params.require(:comment).permit(:user_id, :title, :content, :post_id, :user_name)
     end
 end
