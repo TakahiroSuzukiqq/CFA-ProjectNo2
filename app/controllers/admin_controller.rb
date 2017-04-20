840ac8b29a0dac8ec5273f555428867000b8fafe
Takahiro Suzuki <takahirosuzuki.k010@icloud.com>
2017-04-20 10:59:43 +1000

    initial commit as Pundit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,26 @@
 class AdminController < ApplicationController
   before_action :authorize_user, only: :index
+  before_action :set_supplier, only: [:show, :edit, :update, :destroy]
+
   def index
-    @uers = User.all
+    @supplier = Supplier.new
     @suppliers = Supplier.all
-    @posts = Post.all
-    @comments = Comment.all
   end
 
     private
 
   def authorize_user
     if current_user.user_type != 'admin'
-      redirect_to root_path, notice: 'Access DENIED'
+      redirect_to root_path, notice: 'Login as User'
+    end
+  end
+
+
+  def set_supplier
+    @supplier = Supplier.find(params[:id])
+  end
+  def supplier_params
+    params.require(:supplier).permit(:supplier_name, :supplier_phone_number, :company_number, :verified)
   end
 
 end
