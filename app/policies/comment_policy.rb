0e5da5b55b34ecc086b232723a49ff046ee5a80a
Takahiro Suzuki <takahirosuzuki.k010@icloud.com>
2017-04-23 21:35:55 +1000

    Pundit comment

---
+++ app/policies/comment_policy.rb
@@ -0,0 +1,25 @@
+class CommentPolicy < ApplicationPolicy
+
+    attr_reader :user, :record
+
+    def initialize(user, record)
+      @user = user
+      @record = record
+    end
+
+    def update?
+      # require 'pry'; binding.pry
+      @record.user == @user
+    end
+
+    def permitted_attributes
+      [:user_id, :title, :content, :post_id, :user_name, {images: []}]
+    end
+
+    class Scope < Scope
+      def resolve
+        scope
+      end
+    end
+
+end
